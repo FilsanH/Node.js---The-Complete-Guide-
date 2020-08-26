9b49c1900e963d12a325c1e48431efcf2c2199d7
Filsan Hassan <fh363.cam.ac.uk>
2020-08-26 12:28:23 +0100

    adding user

--- V5/models/user.js
+++ V5/models/user.js
@@ -1,16 +1,26 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb
+const mongodb = require('mongodb')
+//store refernce to object class
+const ObjectId = mongodb.ObjectId
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
-
-module.exports = User;
+class User {
+  constructor(username, email) {
+    this.name = username
+    this.email = email
+  }
+  save() {
+    const db = getDb()
+    return db.collection('users').insertOne(this)
+  }
+  static findById(userId) {
+    const db = getDb()
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) }) // find returns a cursor findOne doesnt return a cursor but the item but only use if know only one exists
+      .then((user) => {
+        console.log(user)
+        return user
+      })
+  }
+}
+module.exports = User
